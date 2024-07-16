bf87fac0d7044913a35161e9adb9d7ff7f172bc2
Eugene_ Kovgan <jugin.kovgan87@gmail.com>
2024-07-16 23:08:45 +0300

    refactor

--- common/common_functions.py
+++ common/common_functions.py
@@ -5,6 +5,7 @@ import os
 from tikapi import TikAPI
 from typing import Any, Dict
 from dataclasses import asdict
+from common.schemas import Document, Video, VideoStats
 
 def get_mongo_client() -> MongoClient:
     mongo_url = os.getenv("MONGO_URL")
@@ -38,26 +39,26 @@ def save_data_to_mongo(collection_name: str, data: Dict[str, Any], ts: str) -> N
 def combine_videos_object(document: Dict[str, Any], platform: str) -> Dict[str, Any]:
     video = document.get("video", {})
     stats = video.get("stats", {})
-    return {
-        "_id": document.get("_id"),
-        "recordCreated": document.get("recordCreated"),
-        "tags": document.get("tags"),
-        "platform": platform,
-        "video": {
-            "author": video.get("author", {}).get("nickname") if platform == "tiktok" else video.get("author_name") if platform == "instagram" else video.get("author") if platform == "youtube" else None,
-            "createTime": video.get("createTime") if platform in "tiktok" else video.get("createTime") if platform == "instagram" else video.get("createTime") if platform == "youtube" else None,
-            "description": video.get("desc") if platform in "tiktok" else video.get("desc") if platform == "instagram" else video.get("desc") if platform == "youtube" else None,
-            "id": video.get("id"),
-            'duration': video.get("music", {}).get("duration") if platform == "tiktok" else None,
-            "video_url": video.get("video", {}).get("playAddr") if platform == "tiktok" else video.get("cover") if platform == "instagram" else video.get("url") if platform == "youtube" else None,
-            "img_url": video.get("video", {}).get("cover") if platform == "tiktok" else video.get("image_url") if platform == "instagram" else video.get("cover") if platform == "youtube" else None,
-            "stats": {
-                "collectCount": stats.get("collectCount") if platform == "tiktok" else None,
-                "commentCount": stats.get("commentCount") if platform == "tiktok" else video.get("comment_count") if platform == "instagram" else None,
-                "diggCount": stats.get("diggCount") if platform == "tiktok" else video.get("like_count") if platform == "instagram" else None,
-                "playCount": stats.get("playCount") if platform == "tiktok" else None,
-                "shareCount": stats.get("shareCount") if platform == "tiktok" else None,
-                "followers_count": None if platform == "tiktok" else video.get("profile_picture_url", {}).get("followers_count") if platform == "instagram" else None,
-            }
-        }
-    }
+    return asdict(Document(
+        _id=document.get("_id"),
+        recordCreated=document.get("recordCreated"),
+        tags=document.get("tags"),
+        platform=platform,
+        video=Video(
+            author=video.get("author", {}).get("nickname") if platform == "tiktok" else video.get("author_name") if platform == "instagram" else video.get("author") if platform == "youtube" else None,
+            createTime=video.get("createTime") if platform in ["tiktok", "youtube", "instagram"] else None,
+            description=video.get("desc") if platform in ["tiktok", "youtube", "instagram"] else None,
+            id=video.get("id"),
+            duration=video.get("music", {}).get("duration") if platform == "tiktok" else None,
+            video_url=video.get("video", {}).get("playAddr") if platform == "tiktok" else video.get("cover") if platform == "instagram" else video.get("url") if platform == "youtube" else None,
+            img_url=video.get("video", {}).get("cover") if platform == "tiktok" else video.get("image_url") if platform == "instagram" else video.get("cover") if platform == "youtube" else None,
+            stats=VideoStats(
+                collectCount=stats.get("collectCount") if platform == "tiktok" else None,
+                commentCount=stats.get("commentCount") if platform == "tiktok" else video.get("comment_count") if platform == "instagram" else None,
+                diggCount=stats.get("diggCount") if platform == "tiktok" else video.get("like_count") if platform == "instagram" else None,
+                playCount=stats.get("playCount") if platform == "tiktok" else None,
+                shareCount=stats.get("shareCount") if platform == "tiktok" else None,
+                followers_count=None if platform == "tiktok" else video.get("profile_picture_url", {}).get("followers_count") if platform == "instagram" else None,
+            )
+        )
+    ))
